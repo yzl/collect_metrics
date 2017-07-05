bb6b47efa7ff74f34b2cbd060b4270bd379d784c
Yvonne Lam <yvonne@chef.io>
2017-07-05 13:09:58 -0700

    Install cloudwatch scripts.
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,23 @@ execute 'populate-instance-info' do
   command '/home/centos/instance_data.sh'
   action :nothing
 end
+
+package %w( unzip perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA )
+
+directory '/opt/cloudwatch_monitoring'
+
+remote_file '/opt/cloudwatch_monitoring/CloudWatchMonitoringScripts-1.2.1.zip' do
+  source 'http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip'
+  mode '0755'
+  notifies :run, 'execute[setup_cloudwatch_scripts]', :immediately
+end
+
+execute 'setup_cloudwatch_scripts' do
+  cwd '/opt/cloudwatch_monitoring'
+  user 'root'
+  command <<-EOH
+    unzip CloudWatchMonitoringScripts-1.2.1.zip
+    crontab -l | { cat; echo "*/5 * * * * /opt/cloudwatch_monitoring/aws-scripts-mon/mon-put-instance-data.pl --mem-util --disk-space-util --disk-path=/ --from-cron"; } | crontab -
+  EOH
+  action :nothing
+end
