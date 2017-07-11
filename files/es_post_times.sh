f6d017ee1472f451900c09f9e7976d70460a2d0c
Yvonne Lam <yvonne@chef.io>
2017-07-11 12:00:11 -0700

    Add error count metric, fix units for post time averages.
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

--- files/es_post_times.sh
+++ files/es_post_times.sh
@@ -10,16 +10,19 @@ source /home/centos/instance_info
 #                    '"$request" $status "$request_time" $body_bytes_sent '
 #                    '"$http_referer" "$http_user_agent" "$upstream_addr" "$upstream_status" "$upstream_response_time" "$http_x_chef_version"
 
-command=`grep bulk /var/log/delivery/nginx/es_proxy.access.log | grep POST | awk '{gsub(/"/, "")} {sum10+=$10; sum17+=$17} END {print sum10/NR "\n" sum17/NR}'`
-mapfile -t es_response_time <<<"$command"
-
-# echo "request_time: ${es_response_time[0]}"
-# echo "upstream_response_time: ${es_response_time[1]}"
-
 per_host_options="--namespace AWS/EC2 --region us-west-2 --dimensions TestId=$test_id,Instance=$instance"
 
-aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostRequestTime --unit Bytes --value ${es_response_time[0]}
-aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostUpstreamResponseTime --unit Bytes --value ${es_response_time[1]}
+# Get average post times and upstream post times
+command=`grep bulk /var/log/delivery/nginx/es_proxy.access.log | grep POST | awk '{gsub(/"/, "")} {sum10+=$10; sum17+=$17} END {print sum10/NR "\n" sum17/NR}'`
+mapfile -t es_response_time <<<"$command"
+# echo "request_time: ${es_response_time[0]}"
+# echo "upstream_response_time: ${es_response_time[1]}"
+aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostRequestTime --unit Milliseconds --value ${es_response_time[0]}
+aws cloudwatch put-metric-data $per_host_options --metric-name 5MinuteAveragePostUpstreamResponseTime --unit Milliseconds --value ${es_response_time[1]}
+
+# Count non-200 return statuses
+error_count=`grep bulk /var/log/delivery/nginx/es_proxy.access.log | grep POST | awk '{split($0, field, "\"") ; print field[12]}' | grep -cv "^2"`
+aws cloudwatch put-metric-data $per_host_options --metric-name Non2xxStatuses --unit Count --value ${error_count}
 
 # zero out the log file
 echo '' > /var/log/delivery/nginx/es_proxy.access.log
