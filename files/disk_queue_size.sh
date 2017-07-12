53cd04579c35d6d5b22edbe93eb2f65abcc16bf9
Yvonne Lam <yvonne@chef.io>
2017-07-12 11:02:09 -0700

    Do not use iostat's first report. https://serverfault.com/questions/620727/iostat-reporting-low-r-s-on-first-line-for-specific-disk-expected-r-s-on-all-fo
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

--- files/disk_queue_size.sh
+++ files/disk_queue_size.sh
@@ -2,7 +2,7 @@
 
 source /home/centos/instance_info
 
-command=`iostat -x -d /dev/xvda | awk '/xvda/ { print $9}'`
+command=`iostat -x 1 -d /dev/xvda count 3 | awk 'NR==7 {print $9}'`
 
 per_host_options="--namespace AWS/EC2 --region us-west-2 --dimensions TestId=$test_id,Instance=$instance"
 
